6827d01097e93ee627419739a5e69408ef2cada5
ERIC CLERMONTET <emc@go4.social>
2023-01-06 11:29:00 +0100

    Dashboard Activity Responsive Grid + Hook

---
+++ src/utils/hooks/resizeObserver.jsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react"
+
+
+export const useResizeObserver = (ref) => {
+  const [dimensions, setDimensions] = useState(null);
+  useEffect(() => {
+    const observeTarget = ref.current;
+    const resizeObserver = new ResizeObserver((entries) => {
+      entries.forEach((entry) => {
+        setDimensions(entry.contentRect);
+      });
+    });
+    resizeObserver.observe(observeTarget);
+    return () => {
+      resizeObserver.unobserve(observeTarget);
+    };
+  }, [ref]);
+  return dimensions;
+};
